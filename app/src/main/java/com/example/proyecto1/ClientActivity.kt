0bf68a58cb93270123898f1e4ce1d643eb9f3b55
Diego Morales Jimenez <69130255+diegomoralesjimenez@users.noreply.github.com>
2023-04-07 21:01:57 -0600

    New

--- app/src/main/java/com/example/proyecto1/ClientActivity.kt
+++ app/src/main/java/com/example/proyecto1/ClientActivity.kt
@@ -3,17 +3,21 @@ package com.example.proyecto1
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.FrameLayout
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import com.example.proyecto1.databinding.ActivityClientBinding
 
 class ClientActivity : AppCompatActivity() {
     private lateinit var binding: ActivityClientBinding
+    private lateinit var clientNameTextView: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityClientBinding.inflate(layoutInflater)
         setContentView(binding.root)
         replaceFragment(Prestamo())
+
+        clientNameTextView = findViewById(R.id.welcome)
 
         binding.navigationLayout.setOnItemSelectedListener {
             when (it.itemId) {
@@ -35,5 +39,4 @@ class ClientActivity : AppCompatActivity() {
         fragmentTransaction.replace(R.id.frame_layout, fragment)
         fragmentTransaction.commit()
     }
-
 }
--- app/src/main/java/com/example/proyecto1/HomeActivity.kt
+++ app/src/main/java/com/example/proyecto1/HomeActivity.kt
@@ -11,12 +11,14 @@ import android.widget.Toast
 import androidx.core.content.ContextCompat
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.ktx.Firebase
 import java.util.*
 
 class HomeActivity : AppCompatActivity() {
 
     private lateinit var auth: FirebaseAuth
+    private lateinit var store: FirebaseFirestore
     private lateinit var usernameEditText: EditText
     private lateinit var passwordEditText: EditText
     private lateinit var loginButton: Button
@@ -26,6 +28,7 @@ class HomeActivity : AppCompatActivity() {
         setContentView(R.layout.activity_home)
 
         auth = Firebase.auth
+        store = FirebaseFirestore.getInstance()
         usernameEditText = findViewById(R.id.username)
         passwordEditText = findViewById(R.id.password)
         loginButton = findViewById(R.id.btn_login)
@@ -47,9 +50,10 @@ class HomeActivity : AppCompatActivity() {
                     if (task.isSuccessful) {
                         Toast.makeText(baseContext, "Authentication success.",
                             Toast.LENGTH_SHORT).show()
-                        val intent = Intent(applicationContext, Admin::class.java)
-                        startActivity(intent)
-                        finish()
+                        val user = auth.currentUser
+                        if (user != null) {
+                            checkUserAccessLevel(user.uid)
+                        }
                     } else {
                         Toast.makeText(baseContext, "Authentication failed.",
                             Toast.LENGTH_SHORT).show()
@@ -60,5 +64,54 @@ class HomeActivity : AppCompatActivity() {
         window.navigationBarColor = ContextCompat.getColor(this, R.color.green)
         window.statusBarColor = ContextCompat.getColor(this, R.color.green)
 
+    }
+
+    private fun checkUserAccessLevel(uid: String) {
+        val docRef = store.collection("Users").document(uid)
+
+        docRef.get().addOnSuccessListener { documentSnapshot ->
+            if (documentSnapshot.exists()) {
+                val role = documentSnapshot.getString("Role")
+                if (!TextUtils.isEmpty(role)) {
+                    when (role) {
+                        "Admin" -> {
+                            val intent = Intent(this@HomeActivity, Admin::class.java)
+                            startActivity(intent)
+                            finish()
+                        }
+                        "Client" -> {
+                            val intent = Intent(this@HomeActivity, ClientActivity::class.java)
+                            startActivity(intent)
+                            finish()
+                        }
+                        else -> {
+                            Toast.makeText(
+                                this@HomeActivity,
+                                "Unknown user role: $role",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                        }
+                    }
+                } else {
+                    Toast.makeText(
+                        this@HomeActivity,
+                        "User role not found",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            } else {
+                Toast.makeText(
+                    this@HomeActivity,
+                    "User document not found",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        }.addOnFailureListener { e ->
+            Toast.makeText(
+                this@HomeActivity,
+                "Error checking user role: " + e.message,
+                Toast.LENGTH_SHORT
+            ).show()
+        }
     }
 }
