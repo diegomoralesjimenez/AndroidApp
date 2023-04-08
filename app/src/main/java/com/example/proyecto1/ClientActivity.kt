95274abd04beeee86fa7ec0743d1a99f049a6563
Diego Morales Jimenez <69130255+diegomoralesjimenez@users.noreply.github.com>
2023-04-08 11:53:18 -0600

    New

--- app/src/main/java/com/example/proyecto1/ClientActivity.kt
+++ app/src/main/java/com/example/proyecto1/ClientActivity.kt
@@ -1,40 +1,71 @@
 package com.example.proyecto1
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.FrameLayout
 import android.widget.TextView
+
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
 import com.example.proyecto1.databinding.ActivityClientBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.firestore.FirebaseFirestore
 
 class ClientActivity : AppCompatActivity() {
     private lateinit var binding: ActivityClientBinding
-    private lateinit var clientNameTextView: TextView
+    private lateinit var toolbar: Toolbar
+    private lateinit var auth: FirebaseAuth
+
+    private val authStateListener = FirebaseAuth.AuthStateListener { firebaseAuth ->
+        val user: FirebaseUser? = firebaseAuth.currentUser
+        if (user != null) {
+            val db = FirebaseFirestore.getInstance()
+            val docRef = db.collection("Users").document(user.uid)
+            docRef.get().addOnSuccessListener { documentSnapshot ->
+                if (documentSnapshot.exists()) {
+                    val displayName = documentSnapshot.getString("Nombre")
+                    toolbar.title = "Bienvenido $displayName"
+                }
+            }
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityClientBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        toolbar = findViewById(R.id.welcome)
+
+        
         replaceFragment(Prestamo())
 
-        clientNameTextView = findViewById(R.id.welcome)
-
-        binding.navigationLayout.setOnItemSelectedListener {
-            when (it.itemId) {
+        binding.navigationLayout.setOnItemSelectedListener { menuItem ->
+            when (menuItem.itemId) {
                 R.id.prestamo -> replaceFragment(Prestamo())
                 R.id.ahorro -> replaceFragment(Ahorro())
                 R.id.cuota -> replaceFragment(Cuota())
                 R.id.personal -> replaceFragment(Personal())
-
-                else -> {
-                }
             }
             true
         }
+
+        auth = FirebaseAuth.getInstance()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        auth.addAuthStateListener(authStateListener)
+    }
+
+    override fun onStop() {
+        super.onStop()
+        auth.removeAuthStateListener(authStateListener)
     }
 
     private fun replaceFragment(fragment: Fragment) {
-        val fragmentManager = supportFragmentManager
+        val fragmentManager: FragmentManager = supportFragmentManager
         val fragmentTransaction = fragmentManager.beginTransaction()
         fragmentTransaction.replace(R.id.frame_layout, fragment)
         fragmentTransaction.commit()
