229ae3a155a9f97fd94c47b664c86238caaa09f1
Diego Morales Jimenez <69130255+diegomoralesjimenez@users.noreply.github.com>
2023-04-08 17:23:56 -0600

    Layout new

--- app/src/main/java/com/example/proyecto1/Admin.kt
+++ app/src/main/java/com/example/proyecto1/Admin.kt
@@ -2,10 +2,31 @@ package com.example.proyecto1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import com.example.proyecto1.databinding.ActivityAdminBinding
 
 class Admin : AppCompatActivity() {
+    private lateinit var binding: ActivityAdminBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_admin)
+        binding = ActivityAdminBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.navigationLayout.setOnItemSelectedListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.cliente -> (replaceFragment(newClient()))
+                R.id.prestamo -> replaceFragment(AsignarPrestamo())
+            }
+            true
+        }
     }
+
+    private fun replaceFragment(fragment: Fragment) {
+        val fragmentManager: FragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.adminLayout, fragment)
+        fragmentTransaction.commit()
+    }
+
 }
